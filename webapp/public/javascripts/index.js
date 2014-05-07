18b64b1d986a83ecf1e59babb38bf51411d63f2f
Sam Decrock <sam.decrock@gmail.com>
2014-05-07 15:13:46 +0200

    basic web interface

---
+++ webapp/public/javascripts/index.js
@@ -0,0 +1,57 @@
+var App = function (options){
+
+	var socket = null;
+
+	var init = function (){
+		initSocket();
+		addHandlers();
+		FastClick.attach(document.body);
+	};
+
+	var initSocket = function (){
+		if(socket) return; // already initialized
+
+		socket = io.connect(window.location.hostname);
+
+		// some debugging statements concerning socket.io
+		socket.on('reconnecting', function(seconds){
+			console.log('reconnecting in ' + seconds + ' seconds');
+		});
+		socket.on('reconnect', function(){
+			console.log('reconnected');
+		});
+		socket.on('reconnect_failed', function(){
+			console.log('failed to reconnect');
+		});
+		socket.on('connect', function() {
+			console.log('socket connected');
+		});
+	};
+
+	var addHandlers = function () {
+		$('.preset').click( onPresetClick );
+	};
+
+	var onPresetClick = function (event){
+		var preset = $(this).attr('data-preset');
+
+		console.log(preset);
+
+		socket.emit('preset.move', preset, function (data) {
+			console.log(data);
+		});
+	};
+
+	return {
+		init: init
+	};
+};
+
+
+
+$(function(){
+	var app = new App();
+	app.init();
+});
+
+
